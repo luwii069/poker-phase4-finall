d30edf0f6522c3ce3a363189e6cccd8b8f9f087b
Aicy-nina <muhikanina@gmail.com>
2024-07-19 09:31:00 +0300

    second

--- client/src/components/game/ComputerMoves.jsx
+++ client/src/components/game/ComputerMoves.jsx
@@ -1,1 +1,29 @@
-//add code here 
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import getToken from '../auth/getToken'
+
+function ComputerMoves({props}) {
+    const [move, setMove] = useState([])
+    let token = getToken()
+    function handleClick(){
+    useEffect(function(){
+        axios.get("http://127.0.0.1:5000/game/computer_moves",{
+          headers:{
+            "Authorization": `Bearer ${token}`
+          }
+           
+
+  
+        })
+    }, [])
+  }
+
+
+  return (
+    <div>
+        <button onSubmit={handleClick} >ComputerMove</button>
+    </div>
+  )
+}
+
+export default ComputerMoves;
